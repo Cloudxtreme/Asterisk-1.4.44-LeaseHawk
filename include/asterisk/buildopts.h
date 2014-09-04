70adcfe4cf8ce538c793bf478b0122d37eec0452
Justin Zimmer <jtzimmer6@gmail.com>
2014-09-04 13:28:58 -0700

    Initial commit to repo

---
+++ include/asterisk/buildopts.h
@@ -0,0 +1,9 @@
+/*
+ * buildopts.h 
+ * Automatically generated
+ */
+
+#define DONT_OPTIMIZE 1
+#define MALLOC_DEBUG 1
+#define LOADABLE_MODULES 1
+#define AST_BUILDOPT_SUM "81df69ac2f01dad7b01986ebe305890d"
